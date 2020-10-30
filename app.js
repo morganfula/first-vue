7024f97cf9bd6fb74d8c2964ecac19f964c93533
morganfula <morgan.fula@me.com>
2020-10-31 00:35:12 +0100

    adding vue

---
+++ app.js
@@ -0,0 +1,13 @@
+// const buttonElement = document.querySelector('button');
+// const inputElement = document.querySelector('input');
+// const listElement = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputElement.value; // store value
+//   const listItemElement = document.createElement('li'); // create a list item (li) Element
+//   listItemElement.textContent = enteredValue; 
+//   listElement.appendChild(listItemElement); // add
+//   inputElement.value = '';
+// }
+
+// buttonElement.addEventListener('click', addGoal);
